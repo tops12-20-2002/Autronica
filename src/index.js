6c2fe782c41bfdf80289ea6f02e08d8be69824a9
Christopher Zarsuelo <christopherzarsuelo028@gmail.com>
2025-11-30 00:20:42 +0800

    Removed Unnecessary Files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client'; // ✅ FIXED
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = createRoot(document.getElementById('root')); // ✅ FIXED
 root.render(
